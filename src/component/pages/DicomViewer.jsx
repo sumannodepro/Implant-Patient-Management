88c98b60fb2b9cdf827125fe4c17757ce201c80f
Venkatesan Chandra Sekaran <venkatesan_dinesh@icloud.com>
2025-02-11 15:22:40 +0530

    Update DicomViewer

--- src/component/pages/DicomViewer.jsx
+++ src/component/pages/DicomViewer.jsx
@@ -4,7 +4,7 @@ const DicomViewer = () => {
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h2>Dicom Viewer</h2>
-      <p>This is a basic React component.</p>
+      <p>This is a Dicom Viewer</p>
     </div>
   );
 };
